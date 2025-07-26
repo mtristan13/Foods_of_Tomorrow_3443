26129de35a4160a816465257915541691a947250
InSomniacOsprey <145494069+InSomniacOsprey@users.noreply.github.com>
2025-07-26 00:02:38 -0500

    Create FavoritesManager.java

---
+++ app/src/main/java/edu/utsa/cs3443/cobras/model/FavoritesManager.java
@@ -0,0 +1,22 @@
+//import com.example.mealapp.models.Recipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FavoritesManager {
+    private static List<Recipe> favoriteRecipes = new ArrayList<>();
+
+    public static void addFavorite(Recipe recipe) {
+        if (!favoriteRecipes.contains(recipe)) {
+            favoriteRecipes.add(recipe);
+        }
+    }
+
+    public static void removeFavorite(Recipe recipe) {
+        favoriteRecipes.remove(recipe);
+    }
+
+    public static List<Recipe> getFavorites() {
+        return new ArrayList<>(favoriteRecipes); // return a copy
+    }
+}
